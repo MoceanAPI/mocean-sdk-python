11370af690c7c7fb0058718978926474ba8f930d
NeoSon <lkloon123@hotmail.com>
2019-07-29 12:24:19 +0800

    fix test

--- tests/modules/voice/test_mccc.py
+++ tests/modules/voice/test_mccc.py
@@ -1,18 +1,11 @@
 from unittest import TestCase
 
-from moceansdk import Mccc, RequiredFieldException
+from moceansdk import Mccc
 
 
 class TestMccc(TestCase):
     def test_mccc_say(self):
         say = Mccc.say()
-
-        try:
-            say.get_request_data()
-            self.fail()
-        except RequiredFieldException:
-            pass
-
         say.set_text('testing text')
         self.assertEqual('testing text', say.get_request_data()['text'])
 
@@ -20,13 +13,6 @@ class TestMccc(TestCase):
 
     def test_mccc_bridge(self):
         bridge = Mccc.bridge()
-
-        try:
-            bridge.get_request_data()
-            self.fail()
-        except RequiredFieldException:
-            pass
-
         bridge.set_to('testing to')
         self.assertEqual('testing to', bridge.get_request_data()['to'])
 
@@ -34,13 +20,6 @@ class TestMccc(TestCase):
 
     def test_mccc_collect(self):
         collect = Mccc.collect()
-
-        try:
-            collect.get_request_data()
-            self.fail()
-        except RequiredFieldException:
-            pass
-
         collect.set_event_url('testing event url')
         self.assertEqual('testing event url', collect.get_request_data()['event-url'])
 
@@ -48,13 +27,6 @@ class TestMccc(TestCase):
 
     def test_mccc_play(self):
         play = Mccc.play()
-
-        try:
-            play.get_request_data()
-            self.fail()
-        except RequiredFieldException:
-            pass
-
         play.set_files('testing file')
         self.assertEqual('testing file', play.get_request_data()['file'])
 
@@ -62,13 +34,6 @@ class TestMccc(TestCase):
 
     def test_mccc_sleep(self):
         sleep = Mccc.sleep()
-
-        try:
-            sleep.get_request_data()
-            self.fail()
-        except RequiredFieldException:
-            pass
-
         sleep.set_duration(10000)
         self.assertEqual(10000, sleep.get_request_data()['duration'])
 
--- tests/modules/voice/test_voice.py
+++ tests/modules/voice/test_voice.py
@@ -1,18 +1,15 @@
 import json
 from unittest import TestCase
-from mockito import when, ANY, verify, unstub
+
+import requests_mock
 
 from moceansdk import RequiredFieldException, McccBuilder, Mccc
-from moceansdk.modules.transmitter import Transmitter
 from tests.testing_utils import TestingUtils
 
 
 class TestVoice(TestCase):
-    def setUp(self):
-        self.client = TestingUtils.get_client_obj()
-
     def test_setter_method(self):
-        voice = self.client.voice
+        voice = TestingUtils.get_client_obj().voice
 
         voice.set_to('test to')
         self.assertIsNotNone(voice._params['mocean-to'])
@@ -31,79 +28,65 @@ class TestVoice(TestCase):
         self.assertEqual('json', voice._params['mocean-resp-format'])
 
         # test multiple call control commands
-        voice = self.client.voice
+        voice = TestingUtils.get_client_obj().voice
         voice.set_call_control_commands([{'action': 'say'}])
         self.assertIsNotNone(voice._params['mocean-call-control-commands'])
         self.assertEqual(json.dumps([{'action': 'say'}]), voice._params['mocean-call-control-commands'])
 
-        voice = self.client.voice
+        voice = TestingUtils.get_client_obj().voice
         builder_params = McccBuilder().add(Mccc.say("hello world"))
         voice.set_call_control_commands(builder_params)
         self.assertIsNotNone(voice._params['mocean-call-control-commands'])
         self.assertEqual(json.dumps(builder_params.build()), voice._params['mocean-call-control-commands'])
 
-        voice = self.client.voice
+        voice = TestingUtils.get_client_obj().voice
         mccc_params = Mccc.say('hello world')
         voice.set_call_control_commands(mccc_params)
         self.assertIsNotNone(voice._params['mocean-call-control-commands'])
         self.assertEqual(json.dumps(McccBuilder().add(mccc_params).build()),
                          voice._params['mocean-call-control-commands'])
 
-    def test_call(self):
-        transmitter_mock = Transmitter()
-        when(transmitter_mock).send(ANY, ANY, ANY).thenReturn('testing only')
+    @requests_mock.Mocker()
+    def test_json_call(self, m):
+        TestingUtils.intercept_mock_request(m, 'voice.json', '/voice/dial')
 
-        client = TestingUtils.get_client_obj(transmitter_mock)
+        client = TestingUtils.get_client_obj()
+        res = client.voice.call({
+            'mocean-to': 'test to'
+        })
 
-        # test is required field set
+        self.assertEqual(res.__str__(), TestingUtils.get_response_string('voice.json'))
+        self.__test_object(res)
+
+        self.assertTrue(m.called)
+
+    @requests_mock.Mocker()
+    def test_xml_call(self, m):
+        TestingUtils.intercept_mock_request(m, 'voice.xml', '/voice/dial')
+
+        client = TestingUtils.get_client_obj()
+        res = client.voice.call({
+            'mocean-to': 'test to',
+            'mocean-resp-format': 'xml'
+        })
+
+        self.assertEqual(res.__str__(), TestingUtils.get_response_string('voice.xml'))
+        self.__test_object(res)
+
+        self.assertTrue(m.called)
+
+    @requests_mock.Mocker()
+    def test_required_field_not_set(self, m):
+        TestingUtils.intercept_mock_request(m, 'voice.json', '/voice/dial')
+
+        client = TestingUtils.get_client_obj()
         try:
             client.voice.call()
             self.fail()
         except RequiredFieldException:
             pass
 
-        self.assertEqual('testing only', client.voice.call({
-            'mocean-to': 'test to',
-            'mocean-call-control-commands': 'test call control commands'
-        }))
-
-        verify(transmitter_mock, times=1).send('get', '/voice/dial', ANY)
-
-        unstub()
-
-    def test_json_response(self):
-        with open(TestingUtils.get_resource_file_path('voice.json'), 'r') as file_handler:
-            file_content = ''.join(file_handler.read().splitlines())
-            transmitter_mock = Transmitter()
-            when(transmitter_mock).send(ANY, ANY, ANY).thenReturn(transmitter_mock.format_response(file_content))
-
-            client = TestingUtils.get_client_obj(transmitter_mock)
-            res = client.voice.call({
-                'mocean-to': 'test to'
-            })
-
-            self.assertEqual(res.__str__(), file_content)
-            self.__test_object(res)
-
-        unstub()
-
-    def test_xml_response(self):
-        with open(TestingUtils.get_resource_file_path('voice.xml'), 'r') as file_handler:
-            file_content = ''.join(file_handler.read().splitlines())
-            transmitter_mock = Transmitter()
-            when(transmitter_mock).send(ANY, ANY, ANY).thenReturn(
-                transmitter_mock.format_response(file_content, '/voice/dial', True)
-            )
-
-            client = TestingUtils.get_client_obj(transmitter_mock)
-            res = client.voice.call({
-                'mocean-to': 'test to'
-            })
-
-            self.assertEqual(res.__str__(), file_content)
-            self.__test_object(res)
-
-        unstub()
+        self.assertFalse(m.called)
 
     def __test_object(self, voice_response):
         self.assertIsInstance(voice_response.toDict(), dict)
