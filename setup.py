d5e43ce97245452e84e897257bdf65eed281343a
tzhongyan <iam@tzhongyan.com>
2021-03-08 12:03:22 +0800

    Bumping python travis build
    Also adding .gitignore for popular IDE

--- setup.py
+++ setup.py
@@ -30,5 +30,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ]
 )
